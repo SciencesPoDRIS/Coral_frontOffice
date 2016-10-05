cc9c2d3f37fa3d83d1dced1b4a0b708ec56b520e
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-05 15:19:01 +0200

    Rename main controller

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@
         $translateProvider.useSanitizeValueStrategy('escape');
     });
 
-    app.controller('eresourcesCtrl', ['$scope', '$translate', function($scope, $translate) {
+    app.controller('ResourcesController', ['$scope', '$translate', function($scope, $translate) {
         $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
         // Language switcher
