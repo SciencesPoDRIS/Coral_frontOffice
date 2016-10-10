cfe4e224ecc1b2e149da6595f78ceab5392e5f32
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-10 19:30:40 +0200

    Add an "all" link to reset the filter by letter

--- views/welcome.js
+++ views/welcome.js
@@ -8,7 +8,11 @@
             $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
             $scope.filterByLetter = function() {
-                $scope.indexVM.query = ejs.MatchQuery('title_fr', $(this)[0].letter).type('phrase_prefix');
+                if($(this)[0].hasOwnProperty('letter')) {
+                    $scope.indexVM.query = ejs.MatchQuery('title_fr', $(this)[0].letter).type('phrase_prefix');
+                } else {
+                    $scope.indexVM.query = ejs.MatchAllQuery();
+                }
             }
         }
     ]);
