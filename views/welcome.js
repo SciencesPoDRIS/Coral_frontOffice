06cc5f836eca6c1367198e2374170e28e97c7bf2
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-19 17:20:09 +0200

    Refactor the "Subjects" tab

--- views/welcome.js
+++ views/welcome.js
@@ -11,7 +11,7 @@
                 body: ejs.Request()
                     .query(ejs.MatchQuery('highlight', '1'))
                     .sort(ejs.Sort('title_fr').order('asc'))
-            }, function (error, response) {
+            }, function(error, response) {
                 $scope.highlighted = response.hits.hits;
             });
 
@@ -21,7 +21,7 @@
                 body: ejs.Request()
                     .query(ejs.MatchQuery('status', 'trial'))
                     .sort(ejs.Sort('title_fr').order('asc'))
-            }, function (error, response) {
+            }, function(error, response) {
                 $scope.trial = response.hits.hits;
             });
 
@@ -31,14 +31,27 @@
                 body: ejs.Request()
                     .query(ejs.MatchQuery('status', 'new'))
                     .sort(ejs.Sort('title_fr').order('asc'))
-            }, function (error, response) {
+            }, function(error, response) {
                 $scope.new = response.hits.hits;
             });
+
+            // Get all subjects
+            es.client.search({
+                    index: 'resource',
+                    size: 1000,
+                    body: ejs.Request()
+                        .agg(ejs.TermsAggregation('subjects')
+                            .field('subjects')
+                            .size(0))
+                },
+                function(error, response) {
+                    $scope.subjects = response.aggregations.subjects.buckets;
+                });
 
             $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
             $scope.filterByLetter = function() {
-                if($(this)[0].hasOwnProperty('letter')) {
+                if ($(this)[0].hasOwnProperty('letter')) {
                     $scope.$parent.indexVM.query = ejs.MatchQuery('title_fr', $(this)[0].letter).type('phrase_prefix');
                 } else {
                     // Reset query term
