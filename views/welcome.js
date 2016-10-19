74fd1f0b39730464355c32e1a556c3b802cf6ef2
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-19 13:34:28 +0200

    Retrieve highlighted resources

--- views/welcome.js
+++ views/welcome.js
@@ -3,11 +3,37 @@
 
     var app = angular.module('eResources.welcome', []);
 
-    app.controller('WelcomeController', ['$scope',
-        function($scope) {
-            // Get the resources 'new' or 'in trial', order by status
-            $scope.$parent.indexVM.query = ejs.BoolQuery().should(ejs.MatchQuery('status', 'trial')).should(ejs.MatchQuery('status', 'new'));
-            $scope.$parent.indexVM.sort = ejs.Sort('status').order('desc');
+    app.controller('WelcomeController', ['$scope', 'es',
+        function($scope, es) {
+            // Get all highlighted resources order by title
+            es.client.search({
+                index: 'resource',
+                body: ejs.Request()
+                    .query(ejs.MatchQuery('highlight', '1'))
+                    .sort(ejs.Sort('title_fr').order('asc'))
+            }, function (error, response) {
+                $scope.highlighted = response.hits.hits;
+            });
+
+            // Get all resources 'in trial' order by title
+            es.client.search({
+                index: 'resource',
+                body: ejs.Request()
+                    .query(ejs.MatchQuery('status', 'trial'))
+                    .sort(ejs.Sort('title_fr').order('asc'))
+            }, function (error, response) {
+                $scope.trial = response.hits.hits;
+            });
+
+            // Get all 'new' resources order by title
+            es.client.search({
+                index: 'resource',
+                body: ejs.Request()
+                    .query(ejs.MatchQuery('status', 'new'))
+                    .sort(ejs.Sort('title_fr').order('asc'))
+            }, function (error, response) {
+                $scope.new = response.hits.hits;
+            });
         }
     ]);
 
