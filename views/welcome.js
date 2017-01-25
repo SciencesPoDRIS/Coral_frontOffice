50e8b9dc4bd63ea7d502bb36de4f11f62122f24a
Anne L'Hôte <anne.lhote@gmail.com>
2017-01-25 11:41:31 +0100

    Skin welcome page

--- views/welcome.js
+++ views/welcome.js
@@ -3,8 +3,8 @@
 
     var app = angular.module('eResources.welcome', ['ngSanitize']);
 
-    app.controller('WelcomeController', ['$scope', 'es', '$routeParams', '$filter', '$timeout',
-        function($scope, es, $routeParams, $filter, $timeout) {
+    app.controller('WelcomeController', ['$scope', 'es', '$routeParams', '$filter', '$timeout', 'addFilterToUrl',
+        function($scope, es, $routeParams, $filter, $timeout, addFilterToUrl) {
             // Config
             var filterSeparator = ':';
             $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -33,7 +33,7 @@
             $scope.sites = {
                 'show': false,
                 'size': 5,
-                'icon': 'keyboard_arrow_right',
+                'icon': 'add',
                 'label': $filter('translate')('MORE')
             };
             $scope.getAggreagtion('sites');
@@ -41,14 +41,14 @@
                 $timeout(function() {
                     $scope.sites.show = !$scope.sites.show;
                     $scope.sites.size = ($scope.sites.show ? 100 : 5);
-                    $scope.sites.icon = ($scope.sites.show ? 'keyboard_arrow_up' : 'keyboard_arrow_right');
+                    $scope.sites.icon = ($scope.sites.show ? 'remove' : 'add');
                     $scope.sites.label = ($scope.sites.show ? $filter('translate')('LESS') : $filter('translate')('MORE'));
                 }, 0);
             }
             $scope.subjects = {
                 'show': false,
                 'size': 5,
-                'icon': 'keyboard_arrow_right',
+                'icon': 'add',
                 'label': $filter('translate')('MORE')
             };
             $scope.getAggreagtion('subjects');
@@ -56,14 +56,14 @@
                 $timeout(function() {
                     $scope.subjects.show = !$scope.subjects.show;
                     $scope.subjects.size = ($scope.subjects.show ? 100 : 5);
-                    $scope.subjects.icon = ($scope.subjects.show ? 'keyboard_arrow_up' : 'keyboard_arrow_right');
+                    $scope.subjects.icon = ($scope.subjects.show ? 'remove' : 'add');
                     $scope.subjects.label = ($scope.subjects.show ? $filter('translate')('LESS') : $filter('translate')('MORE'));
                 }, 0);
             }
             $scope.types = {
                 'show': false,
                 'size': 5,
-                'icon': 'keyboard_arrow_right',
+                'icon': 'add',
                 'label': $filter('translate')('MORE')
             };
             $scope.getAggreagtion('types');
@@ -71,7 +71,7 @@
                 $timeout(function() {
                     $scope.types.show = !$scope.types.show;
                     $scope.types.size = ($scope.types.show ? 100 : 5);
-                    $scope.types.icon = ($scope.types.show ? 'keyboard_arrow_up' : 'keyboard_arrow_right');
+                    $scope.types.icon = ($scope.types.show ? 'remove' : 'add');
                     $scope.types.label = ($scope.types.show ? $filter('translate')('LESS') : $filter('translate')('MORE'));
                 }, 0);
             }
@@ -91,6 +91,8 @@
             // Reset the query's term
             $scope.$parent.indexVM.query = ejs.MatchAllQuery();
 
+            // Set "All" as default selected letter
+            $scope.selectedLetter = '';
             // Fill the filters object according to the route params
             if ($routeParams.letter || $routeParams.query || $routeParams.sites || $routeParams.subjects || $routeParams.types) {
                 // Set the "All" tab as selected
@@ -98,9 +100,9 @@
                 // Fill the query's filters attribute
                 if ($routeParams.letter && $routeParams.letter != '') {
                     // Only one letter is allowed in the request
-                    var letter = $routeParams.letter.split(filterSeparator)[0].toUpperCase();
-                    if ($.inArray(letter, $scope.alphabet) != -1) {
-                        $scope.$parent.indexVM.query = ejs.MatchQuery('title_fr_notanalyzed', letter).type('phrase_prefix');
+                    $scope.selectedLetter = $routeParams.letter.split(filterSeparator)[0].toUpperCase();
+                    if ($.inArray($scope.selectedLetter, $scope.alphabet) != -1) {
+                        $scope.$parent.indexVM.query = ejs.MatchQuery('title_fr_notanalyzed', $scope.selectedLetter).type('phrase_prefix');
                     }
                 }
                 if ($routeParams.sites && $routeParams.sites != '') {
@@ -175,37 +177,6 @@
                     $scope.subjects.facets = response.aggregations.subjects.buckets;
                 });
 
-            $scope.addFilterToUrl = function(facet, value) {
-                // Extract the parameters part of the url
-                var parameters = window.location.href.split('?').length == 1 ? '' : window.location.href.split('?')[1];
-                // Transform this parameters into a JSON Object
-                var parametersJSON = parameters ? JSON.parse('{"' + parameters.replace(/&/g, '","').replace(/=/g, '":"') + '"}', function(key, value) {
-                    return key === "" ? value : decodeURIComponent(value)
-                }) : {};
-                // If facet filter already in the url params, replace or complete
-                if ($.inArray(facet, Object.keys(parametersJSON)) != -1) {
-                    switch (facet) {
-                        case 'letter':
-                            parametersJSON[facet] = value;
-                            break;
-                        case 'query':
-                            parametersJSON[facet] = value;
-                            break;
-                        default:
-                            console.log('Error : this facet is not taken into consideration : ' + facet);
-                            break;
-                    }
-                    // Else create the facet as a new query filter
-                } else {
-                    parametersJSON[facet] = value;
-                }
-                parameters = Object.keys(parametersJSON).map(function(k) {
-                    return encodeURIComponent(k) + '=' + encodeURIComponent(parametersJSON[k])
-                }).join('&');
-                // Redirect to the new url with the added facet
-                window.location.href = window.location.href.split('?')[0] + '?' + parameters;
-            }
-
             $scope.removeFilterFromUrl = function(facet) {
                 // Extract the parameters part of the url
                 var parameters = window.location.href.split('?').length == 1 ? '' : window.location.href.split('?')[1];
@@ -221,6 +192,10 @@
                 }).join('&');
                 // Redirect to the new url with the added facet
                 window.location.href = window.location.href.split('?')[0] + '?' + parameters;
+            }
+
+            $scope.addFilterToUrl = function(facet, value) {
+                addFilterToUrl(facet, value)
             }
 
         }
