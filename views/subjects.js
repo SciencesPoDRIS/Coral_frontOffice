82ccc265773b8a189c34ad108c6b62372744ed94
Anne L'Hôte <anne.lhote@gmail.com>
2017-01-31 16:44:58 +0100

    Complete subjects page

--- views/subjects.js
+++ views/subjects.js
@@ -3,8 +3,20 @@
 
     var app = angular.module('eResources.subjects', []);
 
-    app.controller('SubjectsController', ['$scope',
-        function($scope) {
+    app.controller('SubjectsController', ['$scope', 'es',
+        function($scope, es) {
+            // Get all subjects
+            es.client.search({
+                    index: 'resource',
+                    size: 1000,
+                    body: ejs.Request()
+                        .agg(ejs.TermsAggregation('subjects')
+                            .field('subjects')
+                            .size(0))
+                },
+                function(error, response) {
+                    $scope.subjects = response.aggregations.subjects.buckets;
+                });
         }
     ]);
 
