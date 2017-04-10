3e0f153598c79bde51ffab18450fbe9522f15549
Anne L'Hôte <anne.lhote@gmail.com>
2017-04-10 10:19:03 +0200

    Clean and reindent

--- js/app.js
+++ js/app.js
@@ -1,72 +1,74 @@
-    var app = angular.module('eResources', [
-        'ngMaterial',
-        'ngRoute',
-        'pascalprecht.translate',
-        'elasticsearch',
-        'elasticui',
-        'eResources.filters',
-        'eResources.directives',
-        'eResources.services',
-        'eResources.welcome',
-        'eResources.subjects',
-        'eResources.resources',
-        'eResources.resource'
-    ]);
+'use strict';
 
-    // Set to my local cluster address
-    app.constant('euiHost', 'http://localhost:9200/coral');
+var app = angular.module('eResources', [
+    'ngMaterial',
+    'ngRoute',
+    'pascalprecht.translate',
+    'elasticsearch',
+    'elasticui',
+    'eResources.filters',
+    'eResources.directives',
+    'eResources.services',
+    'eResources.welcome',
+    'eResources.subjects',
+    'eResources.resources',
+    'eResources.resource'
+]);
 
-    // Localization configuration
-    app.config(['$translateProvider',
-        function($translateProvider) {
-            // Enable localStorage to save on bandwidth and loading costs
-            // $translateProvider.useLocalStorage();
-            // Add internationalization by downloading static files
-            $translateProvider.useStaticFilesLoader({
-                prefix: 'languages/',
-                suffix: '.json'
-            });
-            // Default language is french
-            $translateProvider
-                .preferredLanguage('fr')
-                .fallbackLanguage('fr')
-                .useSanitizeValueStrategy(null);
-        }
-    ]);
+// Set to my local cluster address
+app.constant('euiHost', 'http://localhost:9200/coral');
 
-    // Routes configuration
-    app.config(['$routeProvider', '$locationProvider',
-        function($routeProvider, $locationProvider) {
-            $routeProvider.
-            // Welcome Page
-            when('/', {
-                templateUrl: 'views/welcome.html',
-                controller: 'WelcomeController'
-            }).
-            // Subjects page
-            when('/subjects', {
-                templateUrl: 'views/subjects.html',
-                controller: 'SubjectsController'
-            }).
-            // Resources page
-            when('/resources', {
-                templateUrl: 'views/resources.html',
-                controller: 'ResourcesController'
-            }).
-            when('/resource/:resourceId', {
-                templateUrl: 'views/resource.html',
-                controller: 'ResourceController'
-            }).
-            otherwise({
-                redirectTo: '/'
-            });
-        }
-    ]);
+// Localization configuration
+app.config(['$translateProvider',
+    function($translateProvider) {
+        // Enable localStorage to save on bandwidth and loading costs
+        // $translateProvider.useLocalStorage();
+        // Add internationalization by downloading static files
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'dist/languages/',
+            suffix: '.json'
+        });
+        // Default language is french
+        $translateProvider
+            .preferredLanguage('fr')
+            .fallbackLanguage('fr')
+            .useSanitizeValueStrategy(null);
+    }
+]);
 
-    app.config(['$mdThemingProvider',
-        function($mdThemingProvider) {
-            $mdThemingProvider.theme('default')
-                .primaryPalette('grey')
-                .accentPalette('red');
-        }
-    ]);
+// Routes configuration
+app.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+        // Welcome Page
+        when('/', {
+            templateUrl: 'views/welcome.html',
+            controller: 'WelcomeController'
+        }).
+        // Subjects page
+        when('/subjects', {
+            templateUrl: 'views/subjects.html',
+            controller: 'SubjectsController'
+        }).
+        // Resources page
+        when('/resources', {
+            templateUrl: 'views/resources.html',
+            controller: 'ResourcesController'
+        }).
+        when('/resource/:resourceId', {
+            templateUrl: 'views/resource.html',
+            controller: 'ResourceController'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
+]);
+
+app.config(['$mdThemingProvider',
+    function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('grey')
+            .accentPalette('red');
+    }
+]);
